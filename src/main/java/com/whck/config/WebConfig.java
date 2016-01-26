d9144ba272c11cd252b65260d859adc16081f5bd
majy <443428773@qq.com>
2016-01-26 11:31:40 +0800

    配置spring mvc bean Signed-off-by: majy <443428773@qq.com>

---
+++ src/main/java/com/whck/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.whck.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+public class WebConfig {
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setContentType("text/html; charset=utf-8");
+		return viewResolver;
+	}
+
+}
