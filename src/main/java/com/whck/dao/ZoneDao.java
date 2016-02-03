5dd173c7a7e4b5d5705cc05520438cc0b6e832ab
majy <443428773@qq.com>
2016-02-03 11:15:25 +0800

    添加联级查询 Signed-off-by: majy <443428773@qq.com>

--- src/main/java/com/whck/dao/ZoneDao.java
+++ src/main/java/com/whck/dao/ZoneDao.java
@@ -1,9 +1,13 @@
 package com.whck.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.whck.dmo.Zone;
 
 public interface ZoneDao extends JpaRepository<Zone, Integer> {
+
+	List<Zone> findByUser_NameLike(String name);
 
 }
