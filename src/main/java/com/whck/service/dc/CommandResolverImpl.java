1de0f84a6278240252c2550d99b5ace8facf89c2
phil <phil_marx@outlook.com>
2016-03-02 17:17:15 +0800

    Signed-off-by: phil <phil_marx@outlook.com>

---
+++ src/main/java/com/whck/service/dc/CommandResolverImpl.java
@@ -0,0 +1,16 @@
+package com.whck.service.dc;
+
+import org.springframework.stereotype.Service;
+
+import com.whck.dmo.Dc;
+
+@Service
+public class CommandResolverImpl implements CommandResolver{
+
+	@Override
+	public Dc resolve(String command) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
