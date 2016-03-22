27c6ca32060357fc4376b8fdcbe40a45992f964f
phil <phil_marx@outlook.com>
2016-03-22 17:05:35 +0800

    Signed-off-by: phil <phil_marx@outlook.com>

--- src/main/java/com/whck/dmo/Product.java
+++ src/main/java/com/whck/dmo/Product.java
@@ -3,10 +3,11 @@ package com.whck.dmo;
 import java.io.Serializable;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+@Entity
 public class Product implements Serializable {
 
 	private static final long serialVersionUID = 2758517517476132988L;
